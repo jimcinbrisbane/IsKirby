103ddcab86d54fd1af66cebb25e0f886bc6e8f1f
Jim Chi <jimcinbrisbane@pm.me>
2022-07-16 14:29:31 +1000

    to txt

--- toRGB.py
+++ toRGB.py
@@ -1,5 +1,11 @@
 import cv2
+import numpy as np
 
 img = cv2.imread('hac.png', cv2.IMREAD_COLOR)
- 
-print('Image Dimensions :', img.shape)
+# one = np.fromstring(img, dtype=int)
+
+np.savetxt("foo.txt", img.reshape((3,-1)), fmt="%s", header=str(img.shape))
+
+# with open('rgb.txt', 'w') as f:
+#     for line in out_arr:
+#         f.write(line)
