e1295945fbfdb9830c72f63424d4bf8d4277394e
Andrew Clarke <andrewclarke.aron@gmail.com>
2022-07-17 14:02:58 +1000

    fix?????

--- frontend/app.py
+++ frontend/app.py
@@ -1,22 +1,12 @@
-<<<<<<< Updated upstream
-from flask import Flask, render_template, request, redirect, url_for
-=======
 from flask import Flask, render_template, request
 from helper import check_upload_file, get_rgb_value, get_path, CipherAES
 
->>>>>>> Stashed changes
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         file = request.files["fileToUpload"]
-<<<<<<< Updated upstream
-        return render_template('index.html')
-    elif request.method == 'GET':
-        return render_template('index.html')
-
-=======
         check_upload_file(file)
         old_key = get_rgb_value(get_path(file.filename))
         key = CipherAES.get_hash_sha512(old_key)
@@ -24,7 +14,6 @@ def index():
     elif request.method == 'GET':
         return render_template('index.html')
           
->>>>>>> Stashed changes
 @app.route('/confirmation', methods=['GET', 'POST'])
 def confirmation():
     if request.method == 'POST':
